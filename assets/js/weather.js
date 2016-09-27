17bb8d3d29b7762fde911a46d6eff32da47df9eb
Clement Allen <clementallen313@gmail.com>
2016-09-27 19:30:39 +0100

    Fixed active page highlighting

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,5 +1,14 @@
-var strippedPath = this.location.pathname.replace(/\//g, '');
-$('a[href="' + strippedPath + '/"]').parent().addClass('active');
+var url = this.location.pathname;
+var parts = url.split('/');
+var page;
+
+if (parts[parts.length - 1].length === 0) {
+    page = parts[parts.length - 2];
+} else {
+    page = parts[parts.length - 1];
+}
+
+$('a[href*="' + page + '"]').parent().addClass('active');
 
 if(FlashDetect.installed){
     $(".flash-disabled-only").hide();
