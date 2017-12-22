96cc3fc7666275d32932c07f5851a1bf1c64f8c3
Allen, Clement A.teststomasterAllen, Clement A <clement.a.allen@accenture.com>
2017-12-22 22:56:50 +0000

    Updated broken links

--- rasp/index.php
+++ rasp/index.php
@@ -8,44 +8,44 @@ include('../_layout/header.php');
 
             <div class="col-md-6 col-md-offset-3">
                 <p class="lead">RASP Overview</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_main.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_main.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Overview"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_main.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_main.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Overview"/></a>
             </div>
 
             <div class="col-md-6">
                 <p class="lead">Temperature</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_temp.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_temp.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Temperature"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_temp.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_temp.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Temperature"/></a>
             </div>
 
             <div class="col-md-6">
                 <p class="lead">Wind Speed</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_wind.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_wind.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Wind Speed"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_wind.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_wind.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Wind Speed"/></a>
             </div>
 
             <div class="col-md-6">
                 <p class="lead">Wind Direction</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_wind_dir.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_wind_dir.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP WInd Direction"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_wind_dir.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_wind_dir.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP WInd Direction"/></a>
             </div>
 
             <div class="col-md-6">
                 <p class="lead">Cu Potential</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_cu.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_cu.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Cu Potential"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_cu.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_cu.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Cu Potential"/></a>
             </div>
 
             <div class="col-md-6">
                 <p class="lead">Rain</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_rain.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_rain.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Rain"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_rain.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_rain.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP Rain"/></a>
             </div>
 
             <div class="col-md-6">
                 <p class="lead">MSL Pressure</p>
-                <a target="_blank" href="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_press.php?lat=51.871733&lon=-0.551233&region=UK4">
-                <img src="http://rasp.inn.leedsmet.ac.uk/cgi-bin/PHP/blip_press.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP MSL Pressure"/></a>
+                <a target="_blank" href="http://rasp-uk.uk/cgi-bin/PHP/blip_press.php?lat=51.871733&lon=-0.551233&region=UK4">
+                <img src="http://rasp-uk.uk/cgi-bin/PHP/blip_press.php?lat=51.871733&lon=-0.551233&region=UK4" class="img-responsive img-rounded" alt="RASP MSL Pressure"/></a>
             </div>
 
         </div>
