3c086ac0b424b4110cd1bdc2609d05aad3758093
Clement Allen <clementallen313@gmail.com>
2018-10-21 19:45:32 +0100

    added bootstrap js from cdn

--- index.php
+++ index.php
@@ -146,6 +146,7 @@ include( 'assets/weatherdata.php');
     </div><!-- /container -->
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="assets/js/flash_detect.min.js" type="text/javascript"></script>
     <script src="assets/js/weather.js" type="text/javascript"></script>
 
