84e39f22117ad62c8d9a8c6972f3fbb984588f3a
Clement Allen <clementallen313@gmail.com>
2021-06-24 19:42:59 +0100

    Updated links

--- links/index.php
+++ links/index.php
@@ -11,7 +11,7 @@ include '../_layout/header.php';
                     <h1>Soundings</h1>
 
                     <p class="lead">
-                        <a href="http://ready.arl.noaa.gov/READYcmet.php" target="_blank" rel="noopener">NOAA Forecast</a><br />
+                        <a href="https://www.ready.noaa.gov/READYcmet.php" target="_blank" rel="noopener">NOAA Forecast</a><br />
                         <a href="http://weather.uwyo.edu/upperair/sounding.html" target="_blank" rel="noopener">University of Wyoming (Global)</a><br />
                     </p>
 
@@ -20,10 +20,10 @@ include '../_layout/header.php';
                     <h1>Sat Pics</h1>
 
                     <p class="lead">
-                        <a href="http://www.sat24.com/en/gb" target="_blank" rel="noopener">Sat 24 UK</a><br />
-                        <a href="http://www.sat24.com/eu" target="_blank" rel="noopener">Sat 24 Europe</a><br />
-                        <a href="http://www.metoffice.gov.uk/public/weather/observation/map/#?map=SatelliteIR&fcTime=1424251800&zoom=5&lon=1.35&lat=53.60" target="_blank" rel="noopener">Met Office latest IR</a><br />
-                        <a href="http://www.weatheronline.co.uk/ukuksat.htm" target="_blank" rel="noopener">Weather Online UK</a><br />
+                        <a href="https://en.sat24.com/en" target="_blank" rel="noopener">Sat 24 UK</a><br />
+                        <a href="https://en.sat24.com/eu" target="_blank" rel="noopener">Sat 24 Europe</a><br />
+                        <a href="https://www.metoffice.gov.uk/public/weather/observation/map/#?map=SatelliteIR&zoom=5&lon=1.35&lat=53.60" target="_blank" rel="noopener">Met Office latest IR</a><br />
+                        <a href="https://www.weatheronline.co.uk/cgi-app/satellite?LANG=en&CONT=ukuk" target="_blank" rel="noopener">Weather Online UK</a><br />
                     </p>
 
                 </div>
@@ -34,8 +34,8 @@ include '../_layout/header.php';
                     <h1>Actuals</h1>
 
                     <p class="lead">
-                        <a href="http://weatherobs.com/" target="_blank" rel="noopener">Weather Obs</a><br />
-                        <a href="http://www.xcweather.co.uk/" target="_blank" rel="noopener">XC Weather</a><br />
+                        <a href="https://weatherobs.com/" target="_blank" rel="noopener">Weather Obs</a><br />
+                        <a href="https://www.xcweather.co.uk/" target="_blank" rel="noopener">XC Weather</a><br />
                     </p>
 
                     <br />
@@ -44,8 +44,7 @@ include '../_layout/header.php';
 
                     <p class="lead">
                         <a href="http://www.ecmwf.int/en/forecasts/charts/medium/mean-sea-level-pressure-wind-speed-850-hpa-and-geopotential-500-hpa?area=Europe&step=96&relative_archive_date=2014100700&parameter=Wind%20850%20and%20mslp" target="_blank" rel="noopener">ECMWF long range</a><br />
-                        <a href="http://www.weathercharts.org/ukmomslp.htm" target="_blank" rel="noopener">Met Office Synoptics 1</a><br />
-                        <a href="http://www.westwind.ch/?link=ukmb,http://www2.wetter3.de/Fax/,.gif,bracknell%2B00,bracknell%2B24,bracknell%2B36,bracknell%2B48,bracknell%2B60,bracknell%2B72,bracknell%2B84,bracknell%2B96,bracknell%2B108,bracknell%2B120,bracknell%2B132" target="_blank" rel="noopener">Met Office Synoptics 2</a><br />
+                        <a href="https://www.weathercharts.org/ukmomslp.htm" target="_blank" rel="noopener">Met Office Synoptics 1</a><br />
                     </p>
 
                     <br />
@@ -53,7 +52,7 @@ include '../_layout/header.php';
                     <h1>Radar</h1>
 
                     <p class="lead">
-                        <a href="http://www.metoffice.gov.uk/public/weather/observation/map/#?map=Rainfall&zoom=5&lon=-4.00&lat=55.01&fcTime=1424251800" target="_blank" rel="noopener">Met Office latest rainfall</a><br />
+                        <a href="https://www.metoffice.gov.uk/public/weather/observation/map/#?map=Rainfall&zoom=5&lon=-4.00&lat=55.01" target="_blank" rel="noopener">Met Office latest rainfall</a><br />
                         <a href="http://www.raintoday.co.uk/" target="_blank" rel="noopener">Rain Today</a><br />
                     </p>
 
@@ -65,8 +64,9 @@ include '../_layout/header.php';
                     <h1>Gliding Weather</h1>
 
                     <p class="lead">
-                        <a href="http://rasp.stratus.org.uk/index.php" target="_blank" rel="noopener">RASP Blipmap</a><br />
+                        <a href="https://skysight.io" target="_blank" rel="noopener">Skysight</a><br />
                         <a href="https://europe.topmeteo.eu/en-gb/uks/start/" target="_blank" rel="noopener">Top Meteo</a><br />
+                        <a href="http://rasp.stratus.org.uk/index.php" target="_blank" rel="noopener">RASP Blipmap</a><br />
                         <a href="https://soarmet.com/   " target="_blank" rel="noopener">Soar Met (Lasham)</a><br />
                         <a href="http://www.instructor.andrewroch.co.uk/weather.htm" target="_blank" rel="noopener">LGC Weather</a><br />
                     </p>
@@ -77,8 +77,8 @@ include '../_layout/header.php';
 
                     <p class="lead">
                         <a href="https://www.metcheck.com/" target="_blank" rel="noopener">Met Check</a><br />
-                        <a href="http://www.netweather.tv/" target="_blank" rel="noopener">Net Weather</a><br />
-                        <a href="http://www.weatheronline.co.uk/cgi-bin/expertcharts?CONT=ukuk&MODELL=gfs&MODELLTYP=1&VAR=pslv&INFO=1" target="_blank" rel="noopener">Weather Online pressures</a><br />
+                        <a href="https://www.netweather.tv/" target="_blank" rel="noopener">Net Weather</a><br />
+                        <a href="https://www.weatheronline.co.uk/cgi-bin/expertcharts?CONT=ukuk&MODELL=gfs&MODELLTYP=1&VAR=pslv&INFO=11" target="_blank" rel="noopener">Weather Online pressures</a><br />
                     </p>
 
                 </div>
